59204040d65674b1a592fad3c3c4bb76833363f1
gretaancora <118370321+gretaancora@users.noreply.github.com>
2025-02-05 10:52:24 +0100

    added comments

--- src/main/java/it/uniroma2/dicii/ispw/mylib/other/LocalDateAdapter.java
+++ src/main/java/it/uniroma2/dicii/ispw/mylib/other/LocalDateAdapter.java
@@ -6,13 +6,21 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class LocalDateAdapter implements JsonDeserializer<LocalDate>, JsonSerializer<LocalDate> {
+
+    //sringa che rappresenta il formato in cui le LocalDate verranno salvate su file JSON
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
+    /*usiamo solo src, ma menteniamo gli altri sue parameti dell'interfaccia, creiamo un nuovo elemento json che segua il formato descritto in formatter
+    LocalDate.of(2024, 12, 5) -> "5-12-2024"
+     */
     @Override
     public JsonElement serialize(LocalDate src, Type typeOfSrc, JsonSerializationContext context) {
         return new JsonPrimitive(src.format(formatter));
     }
 
+    /*come in serialize, prendiamo l'elemento Json come stringa e lo convertimo in un LocalDate seguendo il pattern descritto in formatter
+    "5-12-2024" -> LocalDate.of(2024, 12, 5)
+     */
     @Override
     public LocalDate deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
         return LocalDate.parse(json.getAsString(), formatter);
